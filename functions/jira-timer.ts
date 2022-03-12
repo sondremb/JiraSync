dab15ab20cf7873fc8deb04d1de3e7e8e444b326
sondremb <sondremartin@hotmail.com>
2022-03-12 12:31:47 +0100

    Debugge netlify-funksjon

--- functions/jira-timer.ts
+++ functions/jira-timer.ts
@@ -2,6 +2,5 @@ import { Handler } from "@netlify/functions";
 import { getJiraTimesheet } from "../src/jira-client";
 
 export const handler: Handler = async (event) => {
-	console.log(event);
 	return getJiraTimesheet(event.body ? JSON.parse(event.body) : null);
 };
--- src/jira-client.ts
+++ src/jira-client.ts
@@ -1,5 +1,5 @@
 import { Response } from "@netlify/functions/dist/function/response";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { DateString } from "./types";
 
 interface JiraRequestParams {
@@ -14,34 +14,63 @@ const delivery_field_name = "customfield_10702";
 export const getJiraTimesheet = async (
 	body: Partial<JiraRequestParams> | null
 ): Promise<Response> => {
-	if (body === null) {
-		return {
-			body: "Missing request body",
-			statusCode: 400,
-		};
-	}
-	const { fromDate, toDate, username, password } = body;
-	if (!username || !password) {
-		return {
-			body: "Missing username or password",
-			statusCode: 401,
-		};
-	}
-	if (!fromDate || !toDate) {
-		return {
-			body: "Missing fromDate or toDate",
-			statusCode: 400,
-		};
-	}
+	try {
+		if (body === null) {
+			return {
+				body: "Missing request body",
+				statusCode: 400,
+			};
+		}
+		const { fromDate, toDate, username, password } = body;
+		if (!username || !password) {
+			return {
+				body: "Missing username or password",
+				statusCode: 401,
+			};
+		}
+		if (!fromDate || !toDate) {
+			return {
+				body: "Missing fromDate or toDate",
+				statusCode: 400,
+			};
+		}
 
-	const url = `https://jira.udir.no/rest/timesheet-gadget/1.0/raw-timesheet.json?startDate=${fromDate}&endDate=${toDate}&moreFields=${delivery_field_name}`;
-	return axios
-		.get(url, {
-			auth: {
-				username: username,
-				password: password,
-			},
-		})
-		.then((res) => ({ statusCode: 200, body: JSON.stringify(res.data) }))
-		.catch((err) => err);
+		const url = `https://jira.udir.no/rest/timesheet-gadget/1.0/raw-timesheet.json?startDate=${fromDate}&endDate=${toDate}&moreFields=${delivery_field_name}`;
+		return axios
+			.get(url, {
+				auth: {
+					username: username,
+					password: password,
+				},
+			})
+			.then((res) => ({ statusCode: 200, body: JSON.stringify(res.data) }))
+			.catch((err: AxiosError) => {
+				if (err.response) {
+					// The request was made and the server responded with a status code
+					// that falls out of the range of 2xx
+					console.log("response");
+					console.log(err.response.data);
+					console.log(err.response.status);
+					console.log(err.response.headers);
+				} else if (err.request) {
+					// The request was made but no response was received
+					// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+					// http.ClientRequest in node.js
+					console.log("request", err.request);
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					console.log("Error", err.message);
+				}
+				console.log(err.config);
+				return { statusCode: 500 };
+			});
+	} catch (e: unknown) {
+		let body = "";
+		if (typeof e === "string") {
+			body = e; // works, `e` narrowed to string
+		} else if (e instanceof Error) {
+			body = e.message; // works, `e` narrowed to Error
+		}
+		return { statusCode: 500, body };
+	}
 };
