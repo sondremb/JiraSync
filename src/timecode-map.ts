123c1a9a1f6e325db84b6bfbcecc9fa8891c19d7
sondremb <sondremartin@hotmail.com>
2022-12-01 20:04:31 +0100

    Fjern mer ubrukt kode

--- src/timecode-map.ts
+++ src/timecode-map.ts
@@ -1,5 +1,5 @@
 import { TimecodeEssentialsDTO } from "./bekk-api/data-contracts";
-import { BekkId, BekkTimecodeMap, Jira } from "./types";
+import { BekkId, Jira } from "./types";
 
 export const FrivilligKompetanseByggingId = 1786;
 
@@ -11,45 +11,6 @@ export enum UdirBekkIds {
 	Systemoversikt = 1002326,
 	UBAS = 1002497,
 }
-
-export const knownBekkTimecodes: BekkTimecodeMap = {
-	[UdirBekkIds.NyGjennomføring]: {
-		id: UdirBekkIds.NyGjennomføring,
-		code: "UTA1077",
-		name: "Nytt gjennomføringssystem",
-		isUdir: true,
-	},
-	[UdirBekkIds.PASEksamen]: {
-		id: UdirBekkIds.PASEksamen,
-		code: "UTA1065",
-		name: "PAS Eksamen forvaltning",
-		isUdir: true,
-	},
-	[UdirBekkIds.PASPrøver]: {
-		id: UdirBekkIds.PASPrøver,
-		code: "UTA1068",
-		name: "PAS Prøver forvaltning",
-		isUdir: true,
-	},
-	[UdirBekkIds.Pålogging]: {
-		id: UdirBekkIds.Pålogging,
-		code: "UTA1067",
-		name: "Påloggingssystemet forvaltning",
-		isUdir: true,
-	},
-	[UdirBekkIds.Systemoversikt]: {
-		id: UdirBekkIds.Systemoversikt,
-		code: "UTA1092",
-		name: "Systemoversikten",
-		isUdir: true,
-	},
-	[UdirBekkIds.UBAS]: {
-		id: UdirBekkIds.UBAS,
-		code: "UTA1097",
-		name: "UBAS",
-		isUdir: true,
-	},
-};
 
 interface TimecodeSelector {
 	selector: (jiraIssue: Jira.Timecode) => boolean;
