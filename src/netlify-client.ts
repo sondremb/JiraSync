8c427788b726c113b474098a75b0fcebaf9c7901
Sondre Bakke <sondremartin@hotmail.com>
2025-09-10 22:32:50 +0200

    Legg inn i netlifyclient

--- src/netlify-client.ts
+++ src/netlify-client.ts
@@ -3,6 +3,7 @@ import { Moment } from "moment";
 import { getJiraCredentials, JiraCredentials } from "./jiraCredentials";
 import { Jira } from "./types";
 import { toDateString } from "./Utils/dateUtils";
+import { JiraIssueResult } from "./jira-client";
 
 export interface NetlifyRequestParams {
 	fromDate: Moment;
@@ -19,6 +20,18 @@ export const NetlifyClient = {
 		return axios.post(".netlify/functions/jira-timer", {
 			fromDate: toDateString(params.fromDate),
 			toDate: toDateString(params.toDate),
+			username: credentials.username,
+			password: credentials.password,
+		});
+	},
+	getIssue: (key: string): Promise<AxiosResponse<JiraIssueResult>> => {
+		const credentials = getJiraCredentials();
+		// TODO
+		if (credentials == null) {
+			throw Error("AAA");
+		}
+		return axios.post(".netlify/functions/jira-issue", {
+			ticketKey: key,
 			username: credentials.username,
 			password: credentials.password,
 		});
