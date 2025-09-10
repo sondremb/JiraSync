8ddae2e07a780165a1b6476a7ff3ab08c6414221
Sondre Bakke <sondremartin@hotmail.com>
2025-09-10 22:34:43 +0200

    Checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteTsconfigPaths()],
+	plugins: [
+		react(),
+		viteTsconfigPaths(),
+		checker({
+			typescript: true,
+			// vi har allerede typescript som en del av build-scriptet, så vi trenger ikke å kjøre det her
+			enableBuild: false,
+		}),
+	],
 });
