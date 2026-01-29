26dc840407be3620a32b8a8c905e9413feda5a3e
Sondre Bakke <sondremartin@hotmail.com>
2026-01-29 12:46:57 +0100

    Hent components i JiraIssue request

--- src/data/issue.ts
+++ src/data/issue.ts
@@ -3,11 +3,13 @@ import { CUSTOM_FIELDS, JiraIssueResult } from "../jira-client";
 export interface JiraIssue {
 	key: string;
 	epicLink: string | null;
+	components: string[];
 }
 
 export function mapIssue(issue: JiraIssueResult): JiraIssue {
 	return {
 		key: issue.key,
 		epicLink: issue.fields[CUSTOM_FIELDS.epicLink] ?? null,
+		components: issue.fields.components?.map((c) => c.id) ?? [],
 	};
 }
