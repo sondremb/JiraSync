5c69758e04c335711b43194398af8b0df418433a
Sondre Bakke <sondremartin@hotmail.com>
2026-03-16 21:51:00 +0100

    Legg til Tanstack Router

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import checker from "vite-plugin-checker";
 import netlify from "@netlify/vite-plugin";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
 		react(),
 		viteTsconfigPaths(),
 		checker({
