b5dcc8a28979895e9cef866878b5ecfc1dfa88b3
Sondre Bakke <sondremartin@hotmail.com>
2026-03-18 09:24:17 +0100

    Flytt ting inn i handler
    Håper dette skal tilgjengeliggjøre DEPLOY_URL i function

--- functions/jira-auth-init.ts
+++ functions/jira-auth-init.ts
@@ -4,23 +4,25 @@ import * as client from "openid-client";
 import { StatusCode } from "../src/Utils/statuscode";
 import cookie from "cookie";
 
-const clientId = process.env.JIRA_CLIENT_ID;
-if (!clientId) {
-	throw new Error("JIRA_CLIENT_ID must be set");
-}
-const clientSecret = process.env.JIRA_CLIENT_SECRET;
-if (!clientSecret) {
-	throw new Error("JIRA_CLIENT_SECRET must be set");
-}
-const selfBaseUrl = process.env.DEPLOY_URL;
-if (!selfBaseUrl) {
-	throw new Error("DEPLOY_URL must be set");
-}
-
-const authBaseUrl = "https://auth.atlassian.com";
-const dicoveryUrl = new URL(`${authBaseUrl}/.well-known/openid-configuration`);
-
 export const handler: Handler = async () => {
+	const clientId = process.env.JIRA_CLIENT_ID;
+	if (!clientId) {
+		throw new Error("JIRA_CLIENT_ID must be set");
+	}
+	const clientSecret = process.env.JIRA_CLIENT_SECRET;
+	if (!clientSecret) {
+		throw new Error("JIRA_CLIENT_SECRET must be set");
+	}
+	const selfBaseUrl = process.env.DEPLOY_URL;
+	if (!selfBaseUrl) {
+		throw new Error("DEPLOY_URL must be set");
+	}
+
+	const authBaseUrl = "https://auth.atlassian.com";
+	const dicoveryUrl = new URL(
+		`${authBaseUrl}/.well-known/openid-configuration`,
+	);
+
 	const config = await client.discovery(dicoveryUrl, clientId, clientSecret);
 
 	const codeVerifier = client.randomPKCECodeVerifier();
