ca8a32a5c64470616def5fbb7b6a7c576fe8f36e
Sondre Bakke <sondremartin@hotmail.com>
2026-03-18 09:27:08 +0100

    please please please

--- functions/jira-auth-init.ts
+++ functions/jira-auth-init.ts
@@ -13,7 +13,7 @@ export const handler: Handler = async () => {
 	if (!clientSecret) {
 		throw new Error("JIRA_CLIENT_SECRET must be set");
 	}
-	const selfBaseUrl = process.env.DEPLOY_URL;
+	const selfBaseUrl = process.env.DEPLOY_PRIME_URL ?? process.env.URL;
 	if (!selfBaseUrl) {
 		throw new Error("DEPLOY_URL must be set");
 	}
